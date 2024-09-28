338aa213302eab85baac49b6f733ee4a6cfbe9ad
Mrlmll <accounts@mrl-mll.de>
2024-09-28 21:00:01 +0200

    Initial commit

---
+++ src/main/java/net/marlin/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.marlin.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
