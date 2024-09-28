1c8ddf7c0d8bb5864024bc9c56dbb858292469cd
Mrlmll <accounts@mrl-mll.de>
2024-09-28 23:43:43 +0200

    Added 2 Items: Pink Garnet and Raw Pink Garnet And Added 2 Blocks: Pink Garnet Block and Pink Garnet Ore

--- src/main/java/net/marlin/tutorialmod/TutorialMod.java
+++ src/main/java/net/marlin/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.marlin.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.marlin.tutorialmod.block.ModBlocks;
+import net.marlin.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
